d148c4ebfe8d02e36430d456a1577819030df960
Uday Abhishek <>
2025-06-11 15:20:18 +0530

    test cases

--- LoginApp/LoginAppTests/LoginAppTests.swift
+++ LoginApp/LoginAppTests/LoginAppTests.swift
@@ -6,23 +6,64 @@
 //
 
 import XCTest
+import Combine
 
 final class LoginAppTests: XCTestCase {
-
+    var viewModel: LoginViewModel!
+    var cancellables: Set<AnyCancellable>!
+    
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        viewModel = LoginViewModel()
+        cancellables = []
+        
     }
-
+    
+    func testLoginSuccess() {
+        let expectation = XCTestExpectation(description: "Login succeeds")
+        
+        viewModel.username = "testuser"
+        viewModel.password = "password"
+        
+        viewModel.$isLoggedIn
+            .dropFirst()
+            .sink { isLoggedIn in
+                if isLoggedIn {
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &cancellables)
+        
+        viewModel.login()
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testLoginFailure() {
+        let expectation = XCTestExpectation(description: "Login fails")
+        
+        viewModel.username = "wronguser"
+        viewModel.password = "wrongpassword"
+        
+        viewModel.$error
+            .dropFirst()
+            .sink { error in
+                if error != nil {
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &cancellables)
+        
+        viewModel.login()
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        viewModel = nil
+        cancellables = nil
+        super.tearDown()
     }
 
     func testPerformanceExample() throws {
@@ -31,5 +72,4 @@ final class LoginAppTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
